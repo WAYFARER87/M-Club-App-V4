794f56f98b147693311578929bda299ffae44c0d
Anton Lindeberg <WAYFARER87@users.noreply.github.com>
2025-09-01 20:34:19 +0500

    feat: add font awesome icons for category markers

--- lib/features/mclub/category_model.dart
+++ lib/features/mclub/category_model.dart
@@ -1,13 +1,15 @@
 class Category {
   final String id;
   final String name;
+  final String? faIcon;
 
-  Category({required this.id, required this.name});
+  Category({required this.id, required this.name, this.faIcon});
 
   factory Category.fromJson(Map<String, dynamic> json) {
     return Category(
       id: json['id']?.toString() ?? '',
       name: json['name']?.toString() ?? '',
+      faIcon: json['faIcon']?.toString(),
     );
   }
 }
