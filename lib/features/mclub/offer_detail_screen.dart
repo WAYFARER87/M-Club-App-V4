6dae4af3034090f346ecc0be61c594657f000c8b
Anton Lindeberg <WAYFARER87@users.noreply.github.com>
2025-09-07 22:20:54 +0500

    Highlight favorite icon

--- lib/features/mclub/offer_detail_screen.dart
+++ lib/features/mclub/offer_detail_screen.dart
@@ -272,7 +272,7 @@ class _OfferDetailScreenState extends State<OfferDetailScreen> {
                 IconButton(
                   icon: Icon(
                     _isFavorite ? Icons.favorite : Icons.favorite_border,
-                    color: iconColor,
+                    color: _isFavorite ? Colors.red : iconColor,
                   ),
                   onPressed: _toggleFavorite,
                   tooltip: 'Избранное',
