57e8343a7ded04115809cbf467e5dfe5e5505bf4
Anton Lindeberg <WAYFARER87@users.noreply.github.com>
2025-09-15 02:13:42 +0500

    Remove redundant JustAudioBackground init

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:just_audio_background/just_audio_background.dart';
 
 import 'app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await JustAudioBackground.init(
-    androidNotificationChannelId: 'm_club_radio_channel',
-    androidNotificationChannelName: 'M-Club Radio',
-    androidNotificationIcon: 'assets/images/Radio_RE_Logo.webp',
-    androidNotificationOngoing: true,
-  );
   runApp(const MyApp());
 }
