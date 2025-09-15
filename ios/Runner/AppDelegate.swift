9a082ccc5a2059dbceac2c7aec839af58e6eab44
Anton Lindeberg <WAYFARER87@users.noreply.github.com>
2025-09-15 06:46:30 +0500

    Fix radio playback initialization and enable iOS control panel

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import Flutter
 import UIKit
 import GoogleMaps
+import AVFoundation
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,13 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    do {
+      try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+      try AVAudioSession.sharedInstance().setActive(true)
+    } catch {
+      print("Failed to configure audio session: \(error)")
+    }
+    application.beginReceivingRemoteControlEvents()
     GMSServices.provideAPIKey("AIzaSyCWGXrDv1nBR5YWb4M2OTFcmwbPX7carIM")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
