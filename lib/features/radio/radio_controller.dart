55b2788e1756aa8737db6a9397aad00b857bc483
Anton Lindeberg <WAYFARER87@users.noreply.github.com>
2025-09-16 19:46:03 +0500

    Ensure audio service guard checks running state

--- lib/features/radio/radio_controller.dart
+++ lib/features/radio/radio_controller.dart
@@ -315,7 +315,7 @@ class RadioController extends ChangeNotifier {
   /// This is useful when the app process restarts and needs to
   /// reconnect to a running background audio service.
   Future<void> ensureAudioService() async {
-    if (AudioService.connected == true && _audioHandler != null) {
+    if (AudioService.running == true && _audioHandler != null) {
       _resetAudioHandlerCompleter();
       _completeAudioHandlerCompleter();
       return;
